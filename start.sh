e0161b9ed4a04c4e0d869d18d57b6d2bb293fbbf
Mykola Beznis <beznosn@gmail.com>
2024-07-18 15:21:29 +0300

    fix script

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-LOGFILE=~/reposter/reposter.log
-
 # Check if there are any running tmux sessions
 if tmux list-sessions &> /dev/null; then
   echo "Closing all existing tmux sessions..."
@@ -11,17 +9,16 @@ fi
 
 # Start a new tmux session and run the setup and script
 echo "Starting a new tmux session..."
-tmux new-session -d -s mysession 
-
-  cd ~/reposter/ || { echo "Failed to change directory to ~/reposter/" | tee -a $LOGFILE; exit 1; }
-  git reset --hard || { echo "Git reset failed" | tee -a $LOGFILE; exit 1; }
-  git pull || { echo "Git pull failed" | tee -a $LOGFILE; exit 1; }
-  rm -rf .venv || { echo "Failed to remove existing virtual environment" | tee -a $LOGFILE; exit 1; }
-  python3 -m venv .venv || { echo "Failed to create virtual environment" | tee -a $LOGFILE; exit 1; }
-  source .venv/bin/activate || { echo "Failed to activate virtual environment" | tee -a $LOGFILE; exit 1; }
-  python --version | tee -a $LOGFILE  # Ensure the version is Python 3.7.16+
-  pip install -r requirements.txt || { echo "Failed to install requirements" | tee -a $LOGFILE; exit 1; }
-  python ~/reposter/reposter.py >> $LOGFILE 2>&1 || { echo "Failed to start reposter.py" | tee -a $LOGFILE; exit 1; }
-
+tmux new-session -d -s mysession <<- 'EOF'
+  cd ~/reposter/
+  git reset --hard
+  git pull
+  rm -rf .venv
+  python3 -m venv .venv
+  source .venv/bin/activate
+  python --version  # Ensure the version is Python 3.7.16+
+  pip install -r requirements.txt
+  python ~/reposter/reposter.py
+EOF
 
 echo "Setup and reposter.py are now running in a new tmux session named 'mysession'."

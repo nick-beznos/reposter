358781364d71383024d255c16a0cb042f86c4d08
Mykola Beznis <beznosn@gmail.com>
2024-07-18 15:22:00 +0300

    Fix script

--- start.sh
+++ start.sh
@@ -9,7 +9,8 @@ fi
 
 # Start a new tmux session and run the setup and script
 echo "Starting a new tmux session..."
-tmux new-session -d -s mysession <<- 'EOF'
+tmux new-session -s mysession 
+
   cd ~/reposter/
   git reset --hard
   git pull
@@ -19,6 +20,5 @@ tmux new-session -d -s mysession <<- 'EOF'
   python --version  # Ensure the version is Python 3.7.16+
   pip install -r requirements.txt
   python ~/reposter/reposter.py
-EOF
 
 echo "Setup and reposter.py are now running in a new tmux session named 'mysession'."
